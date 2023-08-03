7e39ff60972a19be2a7a080370b6076b89dda5a2
I-Sicarius-I <game7profile@gmail.com>
2023-08-03 16:52:56 +0200

    Commiting project in batches part 2

---
+++ Source/Rougelike2DTopDown/Rougelike2DTopDown.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "Rougelike2DTopDown.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Rougelike2DTopDown, "Rougelike2DTopDown" );
