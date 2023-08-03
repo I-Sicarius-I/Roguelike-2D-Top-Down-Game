50ac02c9d7c54879dbe4da7551ec185c430c002a
I-Sicarius-I <game7profile@gmail.com>
2023-08-03 16:44:26 +0200

    Revert "Second Commit - Adding all project files, functionality etc."
    This reverts commit 0d9da0f3385c58d5b281a1c7162b7a68d81b8e32.

--- Source/Rougelike2DTopDown/Rougelike2DTopDown.cpp
+++
@@ -1,6 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "Rougelike2DTopDown.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Rougelike2DTopDown, "Rougelike2DTopDown" );
